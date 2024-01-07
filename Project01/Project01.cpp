9aa08c2f2f00672ffee41cdbd7a93d971ba02650
KrzysiekR-00 <krzysztof.rygol@gmail.com>
2024-01-07 19:44:06 +0100

    Add cmake and vcpkg config files

---
+++ Project01/Project01.cpp
@@ -0,0 +1,16 @@
+﻿// Project01.cpp : Defines the entry point for the application.
+//
+
+#include "Project01.h"
+#include <iostream>
+#include "imgui.h"
+using namespace std;
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	cout << IMGUI_CHECKVERSION() << endl;
+	return 0;
+}
