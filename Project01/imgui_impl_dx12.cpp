77fd30e2d99ac125609399c7c9ab4fb295b779b6
KrzysiekR-00 <krzysztof.rygol@gmail.com>
2024-01-11 11:31:04 +0100

    Link dx libraries in cmake

--- Project01/imgui_impl_dx12.cpp
+++ Project01/imgui_impl_dx12.cpp
@@ -50,8 +50,8 @@
 #include <dxgi1_4.h>
 #include <d3dcompiler.h>
 #ifdef _MSC_VER
-#pragma comment(lib, "d3d12.lib")
-#pragma comment(lib, "dxgi.lib")
+//#pragma comment(lib, "d3d12.lib")
+//#pragma comment(lib, "dxgi.lib")
 #pragma comment(lib, "d3dcompiler") // Automatically link with d3dcompiler.lib as we are using D3DCompile() below.
 #endif
 
