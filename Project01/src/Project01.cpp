becdc9e63d10d1087bfe677a30921af58190b7a1
KrzysiekR-00 <krzysztof.rygol@gmail.com>
2024-01-19 11:18:57 +0100

    Reorganize project files

--- Project01/src/Project01.cpp
+++ Project01/src/Project01.cpp
@@ -28,8 +28,8 @@
 // This define is set in the example .vcxproj file and need to be replicated in your app or by adding it to your imconfig.h file.
 
 #include "imgui.h"
-#include "imgui_impl_win32.h"
-#include "imgui_impl_dx12.h"
+#include "../external/imgui/imgui_impl_win32.h"
+#include "../external/imgui/imgui_impl_dx12.h"
 #include <d3d12.h>
 #include <dxgi1_4.h>
 #include <tchar.h>
